437d9e2b5fd2d8cb1b83e4a96f74c714d73f44b3
Kevin Majesta <kevinivano1205@gmail.com>
2023-10-24 00:19:03 +0700

    [Bab 3] Props dan State

---
+++ datablog.js
@@ -0,0 +1,24 @@
+// datablog.js
+export const categories = [
+    {
+      id: 1,
+      name: 'Bandara',
+      image: 'https://cdn-icons-png.flaticon.com/512/7893/7893979.png',
+    },
+    {
+      id: 2,
+      name: 'Hotel',
+      image: 'https://png.pngtree.com/png-vector/20190521/ourmid/pngtree-hotel-icon-for-personal-and-commercial-use-png-image_1044892.jpg',
+    },
+    {
+      id: 3,
+      name: 'Pelabuhan',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSv1u6Afx2BVdEYdnS4bi8c1i-fb9Q3Gjv88y1OnPSEP1LUvp02gqvFciiKyxXLD7nEmgA&usqp=CAU',
+    },
+    {
+      id: 4,
+      name: 'Villa',
+      image: 'https://static.vecteezy.com/system/resources/thumbnails/006/961/746/small/abstract-and-infinity-house-in-blue-colors-free-vector.jpg',
+    },
+  ];
+  
